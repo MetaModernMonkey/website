02dbf1bb5ce2437f3a85ef6c3569ff3721d35e02
Aaron Beall <contact@abeall.com>
2025-12-08 21:57:05 -0500

    Refactor filter toggle button structure for improved readability and functionality

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -56,15 +56,12 @@ export default function Navigation({ searchQuery, onSearchChange, activeFilter, 
                 >
                   {filter.icon && <filter.icon className="w-4 h-4" aria-hidden />}
                   {filter.label}
-                  <button
-                    key={filter.value}
-                    onClick={() => {
-                      trackFilterSelect(filter.value);
-                      onFilterChange(filter.value);
-                    }}
+                </button>
+              );
+            })}
           </div>
         </div>
-        </div>
+      </div>
     </nav>
   );
 }
